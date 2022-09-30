e1bbad89b309aba0eeb84c635e7bdd6afce6b058
Mike Johnson <mike@mapleprintservices.com>
2022-09-29 23:10:27 -0400

    collected all characters in different arrays and joined to large array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
 // Assignment code here
+const specialCharactersString = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+const lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const digitsString = "1234567890";
+const lowerCaseChars = lowerCaseString.split('');
+const upperCaseChars = upperCaseString.split('');
+const specialChars = specialCharactersString.split('');
+const digitChars = digitsString.split('');
+// console.log(specialChars);
+// console.log(lowerCaseChars);
+// console.log(upperCaseChars);
+// console.log(digitChars);
+var generatePassword = function(){
+  // console.log('hello');
+  characterPool = [];
+  characterPool = characterPool.concat(specialChars);
+  characterPool = characterPool.concat(lowerCaseChars);
+  characterPool = characterPool.concat(upperCaseChars);
+  characterPool = characterPool.concat(digitChars);
 
+
+
+  console.log(characterPool);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +38,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generatePassword()
