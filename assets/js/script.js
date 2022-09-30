7091f7eca68c46de108cefd3b0c375f5f4610cf5
Mike Johnson <mike@mapleprintservices.com>
2022-09-30 00:09:29 -0400

    set up confirms to concat character sets to password character pool

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,47 @@ const digitChars = digitsString.split('');
 var generatePassword = function(){
   // console.log('hello');
   characterPool = [];
-  characterPool = characterPool.concat(specialChars);
-  characterPool = characterPool.concat(lowerCaseChars);
-  characterPool = characterPool.concat(upperCaseChars);
-  characterPool = characterPool.concat(digitChars);
-
-
-
-  console.log(characterPool);
-}
+  // characterPool = characterPool.concat(specialChars);
+  // characterPool = characterPool.concat(lowerCaseChars);
+  // characterPool = characterPool.concat(upperCaseChars);
+  // characterPool = characterPool.concat(digitChars);
+  // console.log(characterPool);
+  if (confirm('Would you like to generate a password?')) {
+      passwordLength = prompt('How many characters?'); 
+      if (isNaN(passwordLength)) {
+        alert('You must enter a number.');
+        return
+        } else {
+          if ((passwordLength > 128) || (passwordLength < 8)) {
+            alert('Password must be between 8 and 128 characters long.');
+          } else {
+            if (confirm('Would you like to include lowercase letters?')) {
+              characterPool = characterPool.concat(lowerCaseChars);
+                console.log(characterPool);
+            }
+            if (confirm('Would you like to include uppercase letters?')) {
+              characterPool = characterPool.concat(upperCaseChars);
+                console.log(characterPool);
+            }
+            if (confirm('Would you like to include numbers?')) {
+              characterPool = characterPool.concat(digitChars);
+                console.log(characterPool);
+            }
+            if (confirm('Would you like to include special characters?')) {
+              characterPool = characterPool.concat(specialChars);
+                console.log(characterPool);
+              }
+            }
+            let securePassword = [];
+            securePassword.length = passwordLength;
+            console.log(securePassword);
+            let randomChar = Math.floor(Math.random() * passwordLength);
+            console.log(randomChar);
+            // Now to assign the randomChar to each index of the securePassword
+      }
+    }
+  }
+            // alert('great')
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -38,4 +70,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-generatePassword()
+// generatePassword()
